4315addc372565f8aed4ef1f2f9fc9adf63ec244
Vedrana <vand@dtu.dk>
2021-11-28 21:13:14 +0100

    added colorsphere

---
+++ Example_colorsphere.py
@@ -0,0 +1,49 @@
+import icosphere
+import colorsphere
+import numpy as np
+
+import matplotlib.pyplot as plt
+import mpl_toolkits.mplot3d 
+ 
+
+nu = 15
+vertices, faces = icosphere.icosphere(nu)
+face_vectors = vertices[faces].sum(axis=1)
+face_vectors /= np.sqrt((face_vectors**2).sum(axis=1, keepdims=True))
+
+
+fig = plt.figure()
+
+
+colorings = [colorsphere.Uno(), 
+             colorsphere.Duo(),
+             colorsphere.Tre(),
+             colorsphere.Ico()]
+
+for i in range(4):
+    
+    ax = fig.add_subplot(2, 2, i+1, projection='3d')                  
+    
+    face_colors = colorings[i](face_vectors)
+    
+    poly = mpl_toolkits.mplot3d.art3d.Poly3DCollection(vertices[faces])
+    poly.set_facecolor(face_colors) 
+    poly.set_edgecolor(None)
+    poly.set_linewidth(0.25)
+    
+    ax.add_collection3d(poly)
+        
+    ax.set_xlim(-1,1)
+    ax.set_ylim(-1,1)
+    ax.set_zlim(-1,1)
+    
+    ax.azim = 40
+    ax.dist = 10
+    ax.elev = 35
+    
+    ax.set_xticks([-1,0,1])
+    ax.set_yticks([-1,0,1])
+    ax.set_zticks([-1,0,1])
+
+
+
---
+++ colorsphere.py
@@ -0,0 +1,150 @@
+import numpy as np
+import matplotlib.cm as c
+
+
+class Coloring():
+    
+    def __init__(self, ordering=None, rotation=None, z_direction=None):
+        self.ordering = ordering
+        self.rotation = rotation
+        if self.rotation is None and z_direction is not None:
+            self.rotation = get_rotation(z_direction)
+    
+    def __call__(self, vectors):
+        
+        if self.ordering is not None:
+            vectors = vectors[:,self.ordering]
+        if self.rotation is not None:
+            vectors =  vectors @ self.rotation.T
+        return self.color(vectors)
+        
+
+class Tre(Coloring):
+    '''
+    Suitable for coloring vectors which are interpretable as: 'mostly x', 
+    'mostly y' or 'mostly z'.'
+    (Tre as for three axes.)
+    '''
+            
+    def color(self, vectors):
+        return np.abs(vectors)
+    
+
+class Uno(Coloring):
+    '''
+    Suitable for coloring vectors predominantly in one (z) direction. 
+    Orientations in orhogonal (x-y) plane are gray. Orientations in 
+    predominant directions are white.
+    (Uno as for unidirectional.)
+    '''
+    
+    def color(self, vectors):
+        
+        s = np.mod(np.arctan2(vectors[:,1], vectors[:,0]) + 
+                   (vectors[:,2]<0)*np.pi, 2*np.pi)   
+        h_pole = vectors[:,2]**2
+        h_pole = h_pole[:,None]
+        h_equator = 1-h_pole
+        colors = c.hsv(s/(2*np.pi))[:,:3]
+        h_pole = h_pole**8
+        colors = colors * (1 - h_pole) + 1 * h_pole
+        h_equator = h_equator**8
+        colors = colors * (1 - h_equator) + 0.25 * h_equator
+        return colors
+
+
+class Duo(Coloring):
+    '''
+    Suitable for coloring vectors predominantly in a x-y plane. Orientations
+    in z direction are gray.
+    (Duo as for two directions i.e. plane.)
+    '''
+           
+    def color(self, vectors):
+        
+        h = vectors[:,2]**2  # no discontinuity and less gray
+        h = h[:,None]
+        s = np.mod(np.arctan2(vectors[:,1], vectors[:,0]), np.pi)
+        colors = c.hsv(s/np.pi)[:,:3] * (1-h) + 0.5*h
+        return colors
+
+
+class Ico(Coloring):
+    '''
+    Suitable for coloring vectors with no known predominant orientations.
+    (Ico as for icosahedron-based.)
+    '''
+    
+    def __init__(self, ordering=None, rotation=None):
+        
+        super().__init__(ordering, rotation)    
+        phi = (1+np.sqrt(5))/2    
+        vertices = np.array([
+            [0, 1, phi], [0,-1, phi], [1, phi, 0],
+            [-1, phi, 0], [phi, 0, 1], [-phi, 0, 1]])/np.sqrt(1 + phi**2)
+        self.vertices = np.concatenate((vertices, -vertices))
+        vertex_colors = np.array([[1, 1, 0, 0, 0, 1], [0, 1, 1, 1, 0, 0],
+                                  [0, 0, 0, 1, 1, 1]]).T
+        self.vertex_colors = np.concatenate((vertex_colors, vertex_colors))
+
+    def color(self, vectors):
+        
+        ijk = np.argsort(vectors @ self.vertices.T, axis=1)[:,-1:-4:-1] # we need only 3 closest centers
+        
+        # baricentric coordinates (A,B,C either all positive or all negative)
+        A = np.cross(self.vertices[ijk[:,1], :], self.vertices[ijk[:,2], :])
+        B = np.cross(self.vertices[ijk[:,2], :], self.vertices[ijk[:,0], :])
+        C = np.cross(self.vertices[ijk[:,0], :], self.vertices[ijk[:,1], :])
+        
+        A = (vectors * A).sum(axis=1, keepdims=True)
+        B = (vectors * B).sum(axis=1, keepdims=True)
+        C = (vectors * C).sum(axis=1, keepdims=True)
+    
+        # # normalization
+        # n = 1/(A+B+C)
+        # A = A*n
+        # B = B*n
+        # C = C*n
+        
+        # attempt with potentions
+        k = 2
+        A = A**k
+        B = B**k
+        C = C**k
+        
+        rgb = (A * self.vertex_colors[ijk[:,0],:] + B * self.vertex_colors[ijk[:,1],:] + 
+                    C * self.vertex_colors[ijk[:,2],:])
+            
+        rgb /= (A+B+C)  # normalization
+        
+        return rgb
+
+
+def get_rotation(z, y=None):
+    '''
+    Given a direction z and optionally y returns an orthogonal coordinate 
+    system, i.e. a rotation matrix transforming identity to [x,y,z].  
+    '''
+    
+    z = np.array(z, dtype=np.double)
+    z /= np.sqrt((z**2).sum())
+    
+    if y is None:
+        y = np.array([0,1,0], dtype=np.double)
+        if abs(np.dot(y,z))>0.9:
+            y = np.array([1,0,0], dtype=np.double)
+    else:   
+        y = np.array(y, dtype=np.double)
+   
+    y = y - np.dot(y,z)*z
+    y /= np.sqrt((y**2).sum())
+    
+    x = np.cross(y,z)
+    
+    return(np.stack((x,y,z)))
+
+
+    
+    
+    
+    
